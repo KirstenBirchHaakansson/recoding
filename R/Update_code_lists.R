ae118650dbcdb3fee8c82386c48209447361e1da
KirstenBirchHaakansson <kih@aqua.dtu.dk>
2018-08-02 18:11:14 +0200

    Fixed missing 0 in lplads codes

--- R/Update_code_lists.R
+++ R/Update_code_lists.R
@@ -2,11 +2,15 @@
 
 Update_code_lists <- function() {
 
+  library(stringr)
+
   #location codes from FST, Denmark
 
   lplads <- read.csv("Q:/mynd/SAS Library/Lplads/lplads.csv", header = T, sep = ",")
+  lplads$start <- as.character(str_pad(start, 5, pad = "0"))
   l_FSTLocCodes <- lplads[, c("start", "harbourEU", "nationl")]
   devtools::use_data(l_FSTLocCodes, overwrite = T, internal = TRUE)
+  devtools::use_data(l_FSTLocCodes, overwrite = T, internal = FALSE)
 
  }
 
