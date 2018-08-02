da9c61a5cf57c07fcfdcdfddb6c264a7ad9216cc
KirstenBirchHaakansson <kih@aqua.dtu.dk>
2018-08-02 18:26:16 +0200

    Changed the name of FST location codes look_up

--- R/Update_code_lists.R
+++ R/Update_code_lists.R
@@ -7,7 +7,7 @@ Update_code_lists <- function() {
   #location codes from FST, Denmark
 
   lplads <- read.csv("Q:/mynd/SAS Library/Lplads/lplads.csv", header = T, sep = ",")
-  lplads$start <- as.character(str_pad(start, 5, pad = "0"))
+  lplads$start <- as.character(str_pad(lplads$start, 5, pad = "0"))
   l_FSTLocCodes <- lplads[, c("start", "harbourEU", "nationl")]
   devtools::use_data(l_FSTLocCodes, overwrite = T, internal = TRUE)
   devtools::use_data(l_FSTLocCodes, overwrite = T, internal = FALSE)
